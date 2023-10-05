b7af829b24da1ecf45b64ef3113cf3289123cdf9
VasilyLevit <vasily.levit@gmail.com>
2023-10-05 20:22:36 +0800

    init

---
+++ task4/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+
+from task4.forms import RegistrationForm
+from task4.models import db, User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+csrf = CSRFProtect(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///users.db'
+db.init_app(app)
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate():
+        username =form.username.data
+        email = form.email.data
+        password = form.password.data
+        existing_user = User.query.filter(
+            (User.username == username) | (User.email == email)
+        ).first()
+        if existing_user:
+            error_msg = 'Username or email already exists.'
+            form.username.errors.append(error_msg)
+            return render_template('register.html', form=form)
+        new_user = User(username=username, email=email, password=password)
+        # вызов метода шифрования пароля (задание 8)
+        new_user.set_password(password) 
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Выводим сообщение об успешной регистрации
+        success_msg = 'Registration successful!'
+        return success_msg
+
+    return render_template('register.html', form=form)
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+# from task1.main import app
+# from task2.main import app
+from task4.main import app
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
